4584ef2d8b3778fb63251445bf6a8fec82c26850
Shiv2701 <27o1shivam@gmail.com>
2023-07-01 16:03:29 +0530

    this is Initial commit

---
+++ Milestone_1/Basic/Helloworld.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+// if we define (using namespace std) here
+
+int main(){
+
+    std::cout<<"HelloWorld"; // we can then use (cout<<"Helloworld";) to print 
+    return 0;
+}
---
+++ Milestone_1/Basic/Input.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int a ;
+    cin>>a;// this command (cin>>) is used for input and (>>) this is insertion operator
+
+    int b ;
+    cin>>b;
+
+    int sum = a+b;
+    cout<<"sum is ";
+    cout<<sum<<endl; // endl can be used as line break instead of \n
+    return 0;
+}
